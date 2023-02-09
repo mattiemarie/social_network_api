be34fc71320cb0e755439258331742aa5307e81b
Mattie Gonzalez <gonzalem@lssc.edu>
2023-02-09 11:17:29 -0500

    added to user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,13 +1,31 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    updateUser,
+    createUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers'); //Might need to add userControllers
+
 //GET all users
+//POST a new user
+router.route('/').get(getUsers).post(createUser);
 
 //GET a single user by its _id and populated thought and friend data
-
-//POST a new user
-
 //PUT to update a user by its _id
-
 //DELETE to remove user by its _id
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
 //POST to add a new friend to users friend list
+router.route('/:userId/friends').post(addFriend);
 
 //DELETE to remove a friend from a user's friend list
+router.route('/:userId/friends/:friendId').delete(removeFriend);
+
+module.exports = router;
