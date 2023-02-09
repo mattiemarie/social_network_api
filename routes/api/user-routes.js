51e7bd73a102c2029aaadd7de9b635878cd92d90
Mattie Gonzalez <gonzalem@lssc.edu>
2023-02-09 18:14:16 -0500

    changes to user-routes.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -11,7 +11,7 @@ const {
 
 //GET all users
 //POST a new user
-router.route('/').get(getUsers).post(createUser);
+router.route('/api/users').get(getUsers).post(createUser);
 
 //GET a single user by its _id and populated thought and friend data
 //PUT to update a user by its _id
@@ -23,9 +23,9 @@ router
     .delete(deleteUser);
 
 //POST to add a new friend to users friend list
-router.route('/:userId/friends').post(addFriend);
+router.route('/api/users/:userId/friends/:friendId').post(addFriend);
 
 //DELETE to remove a friend from a user's friend list
-router.route('/:userId/friends/:friendId').delete(removeFriend);
+router.route('/api/users/:userId/friends/:friendId').delete(removeFriend);
 
 module.exports = router;
