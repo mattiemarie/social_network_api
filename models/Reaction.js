c9098bd59cd9a3aa0aa6f486c826532c278c153b
Mattie Gonzalez <gonzalem@lssc.edu>
2023-02-15 18:20:17 -0500

    adding index.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -30,4 +30,6 @@ const reactionSchema = new Schema(
   //This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
 );
 
-module.exports = reactionSchema;
+const Reaction = model('reaction', reactionSchema)
+
+module.exports = Reaction;
