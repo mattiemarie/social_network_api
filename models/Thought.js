ade4538e495144825c37102bdbd7c8913cf0c0a6
Mattie Gonzalez <gonzalem@lssc.edu>
2023-02-16 17:49:42 -0500

    adding to thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-// const assignmentSchema = require('./Assignment');
+const reactionSchema = require('./Reaction');
 //Change for this assignment
 
 // Schema to create User model
@@ -7,29 +7,35 @@ const thoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
-      required: true,
+      required: 'Text is Required',
       maxlength: 280,
       minlength: 1,
     },
-    createdAt: {
+    userCreated: {
       type: Date,
       default: Date.now,
       //Use getter method to format timestamp on query
     },
     username: {
       type: String,
-      required: true,
+      required: 'Username Created this thought',
     },
-    reactions: {
-        //Array of nested documents created with the reactionSchema
-        },
+
+    //Array of nested documents created with the reactionSchema
+    reactions: [reactionSchema]        
   },
   {
     toJSON: {
       //Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+      virtuals: true,
+      getters: true
     },
-  }
-);
+    id: false
+  });
+
+thoughtSchema.virtural('reactionCount').get(function() {
+  return this.reactions.length;
+});
 
 const Thought = model('thought', thoughtSchema);
 
